384b2786c5598d35633bdd52096bbbd8d054c1c5
Chayse McMillan <chaysemcmillan@Reciprotech.local>
2024-03-19 22:08:26 -0600

    Added custom record selection UI

--- force-app/main/default/lwc/recursiveRecordPicker/recursiveRecordPicker.js
+++ force-app/main/default/lwc/recursiveRecordPicker/recursiveRecordPicker.js
@@ -3,17 +3,21 @@
  * 
  * @author Chayse McMillan
  */
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getRelatedListResults from '@salesforce/apex/RecursiveRecordPickerController.getRelatedListResults';
 
 export default class RecursiveRecordPicker extends LightningElement {
-
-    traversalSelections = [];
+    
+    dataLoaded = false;
+    selectionActive = false;
+    leafNodeSelected = false;
+    picklistFieldText = '';
+    @track picklistRecordOptions = [];
 
     /**
-     * The object api name to perform recursion against -- Ex: "My_Object__c"
+     * REQUIRED: The object api name to perform recursion against -- Ex: "My_Object__c"
      */
     @api set objectApiName(val) {
         this._objectApiName = val;
@@ -24,7 +28,7 @@ export default class RecursiveRecordPicker extends LightningElement {
     }
 
     /**
-     * The display field Api Name (User Facing Traversal Value) -- Ex: "Name" field
+     * REQUIRED: The display field Api Name (User Facing Traversal Value) -- Ex: "Name" field
      */
     @api set displayFieldApiName(val) {
         this._displayFieldApiName = val;
@@ -35,7 +39,7 @@ export default class RecursiveRecordPicker extends LightningElement {
     }
 
     /**
-     * The root node sObject Id to base the recursive selection on -- Ex: "000000000000" (sf record Id)
+     * REQUIRED: The root node sObject Id to base the recursive selection on -- Ex: "000000000000" (sf record Id)
      */
     @api set rootNodeId(val) {
         this._rootNodeId = val;
@@ -46,7 +50,7 @@ export default class RecursiveRecordPicker extends LightningElement {
     }
 
     /**
-     * The field reference on the target object that supports recursion -- Ex: "My_Recursive_Object_Lookup__c"
+     * REQUIRED: The field reference on the target object that supports recursion -- Ex: "My_Recursive_Object_Lookup__c"
      */
     @api set recursiveFieldApiName(val) {
         this._recursiveFieldApiName = val;
@@ -57,44 +61,59 @@ export default class RecursiveRecordPicker extends LightningElement {
     }
 
     /**
+     * OPTIONAL: Picklist Label
+     */
+    @api picklistLabel;
+
+
+
+    get recursiveQueryRequestJSON() {
+        return JSON.stringify({
+            objectApiName: this.objectApiName,
+            displayFieldApiName: this.displayFieldApiName,
+            recursiveFieldApiName: this.recursiveFieldApiName,
+            parentRecordId: this.rootNodeId
+        });
+    }
+
+    /**
      * Wire method to query child records for thec current "rootNodeId"
      */
     _getRelatedListResults;
     @wire(getRelatedListResults, {
-        recursiveQueryRequestJSON: JSON.stringify({
-            objectApiName: '$objectApiName',
-            displayFieldApiName: '$displayFieldApiName',
-            recursiveFieldApiName: '$recursiveFieldApiName',
-            parentRecordId: '$rootNodeId'
-        })
+        recursiveQueryRequestJSON: '$recursiveQueryRequestJSON'
     }) getRelatedListResults(wireResult) {
         this._getRelatedListResults = wireResult;
         const { data, error } = wireResult;
-        if(data) {
-
-            console.log('woo hoo!');
-
-        } else if (error) {
-            console.log('error: '+JSON.stringify(error));
-            this.submitToastNotification('Error Submitting Recursive Query!', JSON.stringify(error), 'error', 'sticky');
+        if(data || error) {
+            if(data) {
+                this.handleInboundRelatedListData(data);
+            } else if (error) {
+                console.log('error: '+JSON.stringify(error));
+                this.submitToastNotification('Error Submitting Recursive Query!', JSON.stringify(error), 'error', 'sticky');
+            }
+            this.dataLoaded = true;
         }
     }
 
-    /**
-     * 
-     * @param {*} data 
-     */
     handleInboundRelatedListData(data) {
+        this.picklistRecordOptions = [];
 
+        // A leaf node was selected -- no further recursion required
+        if(!data || data.length == 0) {
+            this.leafNodeSelected = true;
+            return;
+        }
+
+        // Refresh the picklist options with the selected records child nodes
+        data.forEach((picklistOption) => {
+            this.picklistRecordOptions.push({
+                label: picklistOption[this.displayFieldApiName],
+                value: picklistOption['Id']
+            })
+        });
     }
 
-    /**
-     * 
-     * @param {*} title 
-     * @param {*} message 
-     * @param {*} variant 
-     * @param {*} mode 
-     */
     submitToastNotification(title, message, variant, mode) {
         const event = new ShowToastEvent({
             title: title,
@@ -103,6 +122,28 @@ export default class RecursiveRecordPicker extends LightningElement {
             mode: mode
         });
         this.dispatchEvent(event);
+    }
+
+
+    //
+    // UI Event Handlers
+    //
+    handleRecordSelectionFocus(event) {
+        this.selectionActive = true;
+    }
+
+    handlePicklistSelection(event) {
+
+        // Append to the display traversal
+        if(this.picklistFieldText) {
+            this.picklistFieldText += ' > ';
+        }
+        this.picklistFieldText += event.currentTarget.getAttribute("name");
+
+        // Refresh the search values
+        this.rootNodeId = event.currentTarget.getAttribute("apiname");
+        this.dataLoaded = false;
+        refreshApex(this._getRelatedListResults);
     }
 
 }
---
+++ force-app/main/default/lwc/recursiveRecordPickerDemo/recursiveRecordPickerDemo.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class RecursiveRecordPickerDemo extends LightningElement {
+
+    objectApiName = 'Demo_Object__c';
+    displayFieldApiName = 'Name';
+    rootNodeId = 'a007e00000CUhFdAAL';
+    recursiveFieldApiName = 'Demo_Object_Recursion__c';
+
+    
+}
